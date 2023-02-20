d23ab2a3f46c6fca29d1ccc1b16e2ad0b5ca0815
Ara Periyan <periyan.ara@gmail.com>
2023-02-20 18:26:20 +0400

    add appbar

---
+++ lib/main_page/main_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'spending',
+        ),
+        actions: [
+          IconButton(onPressed: () {}, icon:
+          Container(
+          
+          
+           decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(
+            40,
+          ),
+          border: Border.all(
+            width: 2,
+            color: Color(0x1F77839A) ,
+          ),
+        ),
+        child: Icon(Icons.add),),)
+        ],
+      ),
+    ));
+  }
+}
