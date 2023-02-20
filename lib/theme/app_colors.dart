eb2a520908fcbc057834adfd85aee14186edba55
Ara Periyan <ara.periyan-extern@invia.de>
2023-02-20 18:23:46 +0400

    add theming

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+// main colors
+const Color white = Color(0xFFFFFFFF);
+const Color black = Color(0xFF000000);
+const Color transparent = Colors.transparent;
+
+// text colors
+const Color primaryTextColor = Color(0xFFFFFFFF);
+const Color secondaryTextColor = Color(0xFF77839A);
+
+//app colors
+const Color primaryColor = Color(0xFF132240);
+const Color secondaryColor = Color(0xFF2C4260);
+const Color accentColor1 = Color(0xFF53FDD7);
+const Color accentColor2 = Color(0xFFFF5182);
