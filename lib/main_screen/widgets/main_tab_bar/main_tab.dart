78160ef5c9acf12ec55c8908082ae0cacd0c9b8c
Ara Periyan <periyan.ara@gmail.com>
2023-02-21 15:06:54 +0400

    Connect charts to bloc

--- lib/main_screen/widgets/main_tab_bar/main_tab.dart
+++ lib/main_screen/widgets/main_tab_bar/main_tab.dart
@@ -1,6 +1,8 @@
+import 'package:expenses/main_screen/bloc/main_screen_bloc.dart';
 import 'package:expenses/main_screen/mocks/expenses_list.dart';
 import 'package:expenses/main_screen/models/chart_view_models/bar_chart_view_model.dart';
 import 'package:expenses/main_screen/models/chart_view_models/line_chart_view_model.dart';
+import 'package:expenses/main_screen/models/chart_view_models/pie_chart_view_model.dart';
 import 'package:expenses/main_screen/widgets/custom_bar_chart.dart';
 import 'package:expenses/main_screen/mocks/pie_chart_view_model.dart';
 import 'package:expenses/main_screen/widgets/custom_line_chart.dart';
@@ -9,6 +11,7 @@ import 'package:expenses/main_screen/widgets/tabs/list_tab.dart';
 import 'package:expenses/main_screen/widgets/main_tab_bar/main_tab_bar.dart';
 import 'package:expenses/theme/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MainTab extends StatefulWidget {
   const MainTab({
@@ -57,26 +60,32 @@ class _MainTabState extends State<MainTab> with TickerProviderStateMixin {
                 height: 30,
               ),
               Expanded(
-                child: TabBarView(
-                  controller: _tabController,
-                  children: [
-                    ListTab(),
-                    CustomLineChart(
-                      viewModel: LineChartViewModel.fromExpenses(
-                        expenses,
-                      ),
-                    ),
-                    CustomBarChart(
-                      viewModel: BarChartViewModel.fromExpenses(
-                        expenses,
-                      ),
-                    ),
-                    Center(
-                      child: CustomPieChart(
-                        pieChartViewModel: pieChartViewModel,
-                      ),
-                    )
-                  ],
+                child: BlocBuilder<MainScreenBloc, MainScreenState>(
+                  builder: (context, state) {
+                    return TabBarView(
+                      controller: _tabController,
+                      children: [
+                        ListTab(),
+                        CustomLineChart(
+                          viewModel: LineChartViewModel.fromExpenses(
+                            state.mainScreenViewModel.expenses,
+                          ),
+                        ),
+                        CustomBarChart(
+                          viewModel: BarChartViewModel.fromExpenses(
+                            state.mainScreenViewModel.expenses,
+                          ),
+                        ),
+                        Center(
+                          child: CustomPieChart(
+                            pieChartViewModel: PieChartViewModel.fromExpenses(
+                              state.mainScreenViewModel.expenses,
+                            ),
+                          ),
+                        )
+                      ],
+                    );
+                  },
                 ),
               ),
             ],
