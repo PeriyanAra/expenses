ac8f73df5dd8106bc150e991d9be9bd41862b525
Ara Periyan <periyan.ara@gmail.com>
2023-02-21 10:41:26 +0400

    update ui

--- lib/main_screen/widgets/tabs/list_tab.dart
+++ lib/main_screen/widgets/tabs/list_tab.dart
@@ -1,4 +1,5 @@
 import 'package:expenses/main_screen/mocks/main_screen_view_model_mock.dart';
+import 'package:expenses/theme/export.dart';
 import 'package:flutter/material.dart';
 import 'package:expenses/utils/date_time_convertor.dart'
     show DateTimeFormatterToString;
@@ -14,20 +15,28 @@ class _ListTabState extends State<ListTab> {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: List.generate(
-        mockMainScreenViewModel.expenses.length,
-         (index) {
+      children: List.generate(mockMainScreenViewModel.expenses.length, (index) {
         final expense = mockMainScreenViewModel.expenses[index];
         return Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-           index ==0? Text(
-              expense.date.convertDateToddMMyyyy(),
-            ): SizedBox(),
-            SizedBox(height: 10,),
+            index == 0
+                ? Text(
+                    expense.date.convertDateToddMMyyyy(),
+                    style: TextStyle(color: primaryTextColor),
+                  )
+                : SizedBox(),
+            SizedBox(
+              height: 10,
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: [Text('${expense.name} - ${expense.amount}')],
+              children: [
+                Text(
+                  '${expense.name} - ${expense.amount}\$',
+                  style: TextStyle(color: primaryTextColor),
+                ),
+              ],
             )
           ],
         );
